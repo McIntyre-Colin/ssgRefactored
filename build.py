48b7cd16e7c5ce2f4c2a3b930070ee14cf4005e3
McIntyre-Colin <colinhmcintyre@gmail.com>
2022-04-14 22:07:49 -0400

    base.html incorporated into main()

--- build.py
+++ build.py
@@ -1,14 +1,13 @@
 #Constant across pages
 def main(pages):
-    header = open('./templates/header.html').read()
-    footer = open('./templates/footer.html').read()
-
+    template = open("./templates/base.html").read()
     #for loop to generate each content page from filepaths specified outside of function
     for page in pages:
         content_file = open(page['filename']).read()
         output_file = page['output']
-        
-        combined_file = header + content_file + footer
+        template.replace("{{content}}", content_file)
+
+        combined_file = template.replace("{{content}}", content_file)
         open(output_file, 'w+').write(combined_file)
 
 
