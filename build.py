2872f154fd25a628e6e4b9827e42c6db6b2aafd8
McIntyre-Colin <colinhmcintyre@gmail.com>
2022-04-15 19:30:20 -0400

    added function to replace templates, wokring on active nav bar

--- build.py
+++ build.py
@@ -1,4 +1,19 @@
-#Constant across pages
+#Inserting content into base template
+
+def content_replace(template, content_file, title):
+    combined_file = template.replace("{{content}}", content_file)
+    combined_file = combined_file.replace("{{title}}", title)
+    return combined_file
+
+#Working on function to display the active tab by changing the css it recieves
+
+# def navbar_status(combined_file, title):
+#         combined_file = combined_file.replace("{{nav_status_",title"}}", "class = 'nav-active-tab'")
+#     else:
+#         combined_file = combined_file.replace("{{nav_status",['']}}, "class = 'nav-item")
+
+
+#Outputing finished files
 def main(pages):
     template = open("./templates/base.html").read()
     #for loop to generate each content page from filepaths specified outside of function
@@ -6,10 +21,9 @@ def main(pages):
         content_file = open(page['filename']).read()
         title = page['title']
         output_file = page['output']
-        template.replace("{{content}}", content_file)
-
-        combined_file = template.replace("{{content}}", content_file)
-        combined_file = combined_file.replace("{{title}}", title)
+        combined_file = content_replace(template, content_file, title)
+        
+        # combined_file = combined_file.replace({{title}}, 'nav-active-tab')
         open(output_file, 'w+').write(combined_file)
 
 
@@ -23,13 +37,13 @@ pages = [
     {
     "filename": "./content/projects.html",
     "output": "./docs/projects.html",
-    "title": "My Projects",
+    "title": "Projects",
     },
 
     {
     "filename": "./content/resume.html",
     "output": "./docs/resume.html",
-    "title": "My Resume",
+    "title": "Resume",
 
     }
 
